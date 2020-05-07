773992de1b9a6f028f17a148f7e13279105255c1
deng <deng@dengtekiMacBook-Pro.local>
2020-05-07 18:33:03 +0800

    使用table_name_prefix

--- app/models/writer/application_record.rb
+++ app/models/writer/application_record.rb
@@ -1,5 +1,11 @@
 class Writer::ApplicationRecord < ApplicationRecord
   self.abstract_class = true
 
+  def self.table_name_prefix
+    database_name = WRITER_DATABASE['database']
+    "#{database_name}."
+  end
+
+
   establish_connection(WRITER_DATABASE)
 end
--- app/models/writer/writer.rb
+++ app/models/writer/writer.rb
@@ -1,4 +1,3 @@
 class Writer::Writer < Writer::ApplicationRecord
   has_many :blogs
-  self.table_name = "#{WRITER_DATABASE["database"]}.writers"
 end
