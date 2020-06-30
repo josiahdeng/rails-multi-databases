01451a28e3c8f304246e61829a70aafb990aff05
deng <17607003651@163.com>
2020-06-30 15:13:56 +0800

    删除邮箱信息

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,15 +51,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => 'smtp.163.com',
-      :domain => '163.com',
-      :port => 465,
-      :authentication => 'plain',
-      :user_name => '17607003651@163.com',
-      :password => 'WPKMSGERNNWXMACI'
-  }
 end
